43601435a98596c4794eff54a8fad880116da6af
Vitalii Korol <korolvitalik@gmail.com>
2021-06-08 12:52:38 +0200

    added result table, state, helper function to generate state

--- src/application.js
+++ src/application.js
@@ -1,36 +1,39 @@
-import renderItems from './view';
+import { renderItems, renderCategories } from './view';
+import getItems from './data';
 
 export default () => {
+  const items = getItems();
+  const checkActiveNotes = (noteList) => noteList.filter(({ active }) => active);
+
+  const checkArchiveNotes = (noteList) => noteList.filter(({ active }) => !active);
+
+  const categories = (noteList) => noteList
+    .reduce((acc, { category }) => (acc.includes(category) ? acc : [...acc, category]), []);
+
   const state = {
-    items: [
-      {
-        name: 'Shopping List', created: 'April 20, 2021', category: 'Task', content: 'Tomatoes, bread', dates: [{ currentDate: '', changedDate: '' }], icons: [],
-      },
-      {
-        name: 'The theory of evolution', created: 'April 27, 2021', category: 'Random Thougth', content: 'The evolution', dates: [{ currentDate: '', changedDate: '' }], icons: [],
-      },
-      {
-        name: 'New Feature', created: 'May 05, 2021', category: 'Idea', content: 'Implement new..', dates: [{ currentDate: '3/5/2021', changedDate: '5/5/2021' }], icons: [],
-      },
-      {
-        name: 'William Gaddis', created: 'May 07, 2021', category: 'Quote', content: 'Power doesnt.. ', dates: [{ currentDate: '', changedDate: '' }], icons: [],
-      },
-      {
-        name: 'Books', created: 'May 15, 2021', category: 'Task', content: 'The learn startup', dates: [{ currentDate: '', changedDate: '' }], icons: [],
-      },
-      {
-        name: 'Notes', created: 'April 20, 2021', category: 'Task', content: 'Wrire poem', dates: [{ currentDate: '', changedDate: '' }], icons: [],
-      },
-      {
-        name: 'Film', created: 'April 22, 2021', category: 'Task', content: 'Maxtrix, 1+1...', dates: [{ currentDate: '', changedDate: '' }], icons: [],
-      },
-    ],
+    categories: [],
+    activeNotes: [],
+    archiveNotes: [],
   };
 
-  if (state.items.length !== 0) {
-    renderItems(state.items);
+  if (checkActiveNotes(items)) {
+    state.activeNotes = checkActiveNotes(items);
+    // renderActiveNotes();
+  }
+  if (checkArchiveNotes(items)) {
+    state.archiveNotes = checkArchiveNotes(items);
+    // renderArchiveNotes();
+  }
+  if (categories(items)) {
+    state.categories = categories(items);
+    renderCategories(state.categories);
+  }
+
+  // console.log(state);
+  if (items.length !== 0) {
+    renderItems(items);
   } else {
     return null;
   }
-  return null;
+  return state;
 };
--- src/view.js
+++ src/view.js
@@ -6,7 +6,6 @@ const renderItems = (items) => {
     const {
       name, created, category, content, dates,
     } = item;
-    console.log(dates[0].currentDate);
     const li = document.createElement('li');
     const divCol1 = document.createElement('div');
     const divCol2 = document.createElement('div');
@@ -14,7 +13,9 @@ const renderItems = (items) => {
     const divCol4 = document.createElement('div');
     const divCol5 = document.createElement('div');
     const divCol6 = document.createElement('div');
-    const i = document.createElement('i');
+    const iArchive = document.createElement('i');
+    const iBasket = document.createElement('i');
+    const iEdit = document.createElement('i');
     li.classList.add('table-row');
     divCol1.classList.add('col', 'col-1');
     divCol2.classList.add('col', 'col-2');
@@ -22,20 +23,26 @@ const renderItems = (items) => {
     divCol4.classList.add('col', 'col-4');
     divCol5.classList.add('col', 'col-5');
     divCol6.classList.add('col', 'col-6');
-    i.classList.add('fas', 'fa-cat');
+    iArchive.classList.add('large', 'material-icons');
+    iBasket.classList.add('large', 'material-icons');
+    iEdit.classList.add('large', 'material-icons');
     divCol1.setAttribute('data-label', 'Name');
     divCol2.setAttribute('data-label', 'Created');
     divCol3.setAttribute('data-label', 'Category');
     divCol4.setAttribute('data-label', 'Content');
     divCol5.setAttribute('data-label', 'Dates');
     divCol6.setAttribute('data-label', 'Icon');
+    iArchive.textContent = 'archive';
+    iBasket.textContent = 'delete';
+    iEdit.textContent = 'edit';
     divCol1.textContent = name;
     divCol2.textContent = created;
     divCol3.textContent = category;
     divCol4.textContent = content;
     divCol5.textContent = `${dates[0].currentDate} ${dates[0].changedDate}`;
-    // divCol6.textContent = icons;
-    divCol6.append(i);
+    divCol6.append(iEdit);
+    divCol6.append(iArchive);
+    divCol6.append(iBasket);
     li.append(divCol1);
     li.append(divCol2);
     li.append(divCol3);
@@ -46,4 +53,8 @@ const renderItems = (items) => {
   });
 };
 
-export default renderItems;
+const renderCategories = (categories) => {
+  console.log(categories);
+};
+
+export { renderItems, renderCategories };
